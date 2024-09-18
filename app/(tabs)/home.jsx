4ce50e92099147c1a96c6ca3fd7e5be9335bf375
edenlisk <nkumbuyenock@gmail.com>
2024-09-18 11:10:45 +0200

    Add home images

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,22 +1,27 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+
+import {View, Text, SafeAreaView, Image, ScrollView} from 'react-native'
 import React from 'react'
 
 const Home = () => {
-  return (
-    <SafeAreaView>
+    return (
+        <SafeAreaView style={{paddingVertical: 30}}>
 
-    <Text>Addenda</Text>
-    <ScrollView contentContainerStyle={{paddingVertical:10,gap:25}} >
-        <View className='bg-purple-300 w-full h-[250px]'>
-            <Text>Placeholder</Text>
-        </View>
-        <View className='bg-purple-300 w-full h-[250px]'>
-            <Text>Placeholder</Text>
-        </View>
-  </ScrollView>
-      
-    </SafeAreaView>
-  )
+            <View className="flex items-center justify-center">
+                <Text className="text-2xl font-semibold">Addenda</Text>
+            </View>
+            <ScrollView contentContainerStyle={{paddingVertical: 10, gap: 25}}>
+                <View className='bg-purple-300 w-full h-[250px]'>
+                    <Image source={require('../../assets/blackperson.png')} style={{flex: 1, width: '100%', height: '100%', alignSelf: 'center'}} />
+                    {/*<Text>Placeholder</Text>*/}
+                </View>
+                <View className='bg-purple-300 w-full h-[250px]'>
+                    <Image source={require('../../assets/main.png')} style={{flex: 1,width: '100%', height: '100%', alignSelf: 'center'}} />
+                    {/*<Text>Placeholder</Text>*/}
+                </View>
+            </ScrollView>
+
+        </SafeAreaView>
+    )
 }
 
 export default Home
